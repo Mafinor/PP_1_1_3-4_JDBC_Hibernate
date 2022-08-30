95c5d77a17db708fb3d40bacc238bfc3a183ef7f
Mafinor <i.mafinor@gmail.com>
2022-08-30 11:15:13 +0300

    Transactions are not realised, fixed Util class to match standards

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,11 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Util {
+public final class Util {
     // реализуйте настройку соеденения с БД
     private static final String URL = "jdbc:mysql://localhost:3306/first_db";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
+
+    private Util() {
+
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
